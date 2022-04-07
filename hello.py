00c77bdcfd52bb1f6fb43c1269dd18e3805289e8
“Jen <Jen.j.eyring@gmail.com>
2022-04-07 15:23:09 -0500

    learning hello program

---
+++ hello.py
@@ -0,0 +1,9 @@
+# comment character
+# command + / turns comments on or off
+# print ('hello!)
+
+#prompt the user for their name
+#input is a function that prints what is in the parenthesis and returns the input
+name = input('Hello, what is your name?')
+
+print("hello", name)
