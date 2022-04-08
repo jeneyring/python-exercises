1d54c8ce2c01f3d800bf522714c14a6aea7b993a
“Jen <Jen.j.eyring@gmail.com>
2022-04-08 13:23:25 -0500

    figuring out exercises

---
+++ data_types_and_variables.py
@@ -0,0 +1,14 @@
+#You have rented some movies for your kids: 
+# ## The little mermaid (for 3 days), 
+# ## Brother Bear (for 5 days, they love it), 
+# ## and Hercules (1 day, you don't know yet if they're going to like it). 
+# If price for a movie per day is 3 dollars, how much will you have to pay?
+
+rented_movies = ['Brother_Bear', 'The Little Mermaid', 'Hercules']
+
+price_per_day = 3.00
+
+rented_movies= ['Brother_Bear'= price_per_day * 5) + ('The Little Mermaid'= price_per_day * 3) + ('Hercules' = price_per_day * 1)]
+
+price_per_day
+
