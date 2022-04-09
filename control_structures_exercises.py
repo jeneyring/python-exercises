621c7b075001dec84338edffaf2c8060deca9d76
“Jen <Jen.j.eyring@gmail.com>
2022-04-08 22:24:48 -0500

    working on python

--- control_structures_exercises.py
+++ control_structures_exercises.py
@@ -10,11 +10,9 @@ while user_input != happy_monday:
     user_input = input('Monday' or 'monday')
 
 print('YES! Thank God it is Monday!')
-.
-.
-.
-.
-.
+
+
+
 ## b) prompt the user for a day of the week, print out whether the day is a weekday or a weekend
 
 weekday = ['Monday', 'Tuesday','Wednesday','Thursday','Friday']
@@ -27,11 +25,7 @@ while user_input in weekday:
 else:
     print('Yay! The weekend is here!')
 
-.
-.
-.
-.
-.
+
 ## c)create variables and make up values for:
 #--the number of hours worked in one week
 #--the hourly rate
@@ -49,11 +43,7 @@ if int(hours)> 40:
 else:
     print("Weekly pay is:", hours * rate)
 
-.
-.
-.
-.
-.
+
 #2) LOOP exercises______________________________________________________________________________
 
 ###Create an integer variable i with a value of 5.
@@ -65,8 +55,6 @@ while i <= 15:
     print(i)
     i = i + 1
 
-.
-.
 
 ###Create a while loop that will count by 2's starting with 0 and ending at 100. 
 #Follow each number with a new line.
@@ -76,8 +64,7 @@ while i in range(1,101):
         print(i)
         i = i * 2
 
-.
-.
+
 
 ###Alter your loop to count backwards by 5's from 100 to -10.
 i = 100
@@ -86,8 +73,7 @@ while i >= -10:
     print(i)
     i = i - 5
 
-.
-.
+
 ###Create a while loop that starts at 2, 
 # and displays the number squared on each line while the number is less than 1,000,000. 
 i = 2
@@ -96,8 +82,8 @@ while i <= 1000000:
     print(i)
     i = i **2
 
-.
-.
+
+
 ###Write a loop that uses print to create the output shown below.
 i = 100
 
@@ -105,6 +91,32 @@ while i >= 5:
     print(i)
     i = i - 5
 
-.
-.
+### 2: b) Write some code that prompts the user for a number, 
+# then shows a multiplication table up through 10 for that number.
+
+num = int(input("Choose a number:  "))
+i = 1
+
+while i<=10:
+    print(num, "X", i, "=", num * i)
+    i = i+1
+
+### Create a for loop that uses print to create the output shown below:
+n = int(9)
+
+for i in range(1,n+1):
+    for j in range(1, i+1):
+        print(i, end="")
+    print()
+
+
+##Prompt the user for an odd number between 1 and 50
+#  Use a loop and a break statement to continue prompting the user
+#  if they enter invalid input. 
+# (Hint: use the isdigit method on strings to determine this).
+
+
+
+#  Use a loop and the continue statement to output all the 
+# odd numbers between 1 and 50, except for the number the user entered.
 
