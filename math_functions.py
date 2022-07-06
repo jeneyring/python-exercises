2c207a89e764b7f3404554f6b51ca7bf5ab1cc89
“Jen <Jen.j.eyring@gmail.com>
2022-07-06 11:45:21 -0500

    brushing up python

---
+++ math_functions.py
@@ -0,0 +1,45 @@
+###################################
+#This file follows along with the Real Python book "Automate with Boring stuff"
+###################################
+
+"""Math Operators"""
+#Exponents **
+2 ** 3
+
+#Modules/Remainder %
+20 % 11
+
+#Integer division/Floored quotient //
+22 // 8
+
+#Dividing /
+8 / 2
+
+#Multiplication *
+3 * 6
+
+#Subtraction -
+7 - 4
+
+#Addition +
+3 + 4
+
+"""Order of Operations"""
+# ** is first, then any *, /, //, %...etc
+2 + 3 * 6
+
+(2+3) * 6
+
+48567 * 57834
+
+2 ** 8
+
+23 / 7
+
+23 // 7
+
+23 % 7
+
+(5 - 1) * ((7 + 1) / (3 - 1))
+
+
